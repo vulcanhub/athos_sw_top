1aebf1150ce8bc6029c2b2a3f89c7dd660358f97
Wilson Chan <wilsonxchan@gmail.com>
2022-05-12 18:48:21 -0700

    Fixing include.

--- sw/autogen/top_athos.c
+++ sw/autogen/top_athos.c
@@ -2,7 +2,7 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-#include "top_athos_sw/sw/autogen/top_athos.h"
+#include "top/sw/autogen/top_athos.h"
 
 /**
  * PLIC Interrupt Source to Peripheral Map
